de93afc4f9df64f5b38b069ab5126d9966d2b5f2
Ramin Khalouie <ra.khalouie@gmail.com>
2024-11-22 11:19:16 +0330

    [ADD] : Add nav menu
    Add nav menu for big device sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     extend: {
       fontFamily: {
         primary: ["Yekan", "Arial", "Helvetica Neue", "Helvetica"]
+      },
+      boxShadow:{
+        'menu': '0 -8px 0 0 #f8e88b,0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)'
       }
     },
   },
